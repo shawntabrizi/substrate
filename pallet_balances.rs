c626621f8afa0fd963826470665f9c9d5e5f1d74
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-09-22 23:18:18 +0200

    cargo run --release --features runtime-benchmarks --manifest-path bin/node/cli/Cargo.toml -- benchmark --chain dev --steps 50 --repeat 20 --extrinsic * --raw --execution=wasm --wasm-execution=compiled --output --pallet pallet_balances

--- pallet_balances.rs
+++ pallet_balances.rs
@@ -8,27 +8,27 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl pallet_balances::WeightInfo for WeightInfo {
 	fn transfer() -> Weight {
-		(172_000_000 as Weight)
+		(169_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(1 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
 	fn transfer_keep_alive() -> Weight {
-		(108_000_000 as Weight)
+		(105_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(1 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
 	fn set_balance_creating() -> Weight {
-		(55_000_000 as Weight)
+		(52_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(1 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
 	fn set_balance_killing() -> Weight {
-		(65_000_000 as Weight)
+		(63_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(1 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
 	fn force_transfer() -> Weight {
-		(133_000_000 as Weight)
+		(128_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(2 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
